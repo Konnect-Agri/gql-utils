17eeb68e7d9d4dc17a05f6f554900d290be8dc65
Anshul <malikanshul29@gmail.com>
2022-09-12 17:39:29 +0400

    lint

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,54 @@
-import { parse, Entity } from "./model" 
+import { parse, Entity } from './model';
 // import diff from 'microdiff';
 
 // KONNECT: Gatekeeper
 export const isSubset = (A: string, B: string): boolean => {
   const astA = parse(A);
   const astB = parse(B);
-  let subset = true
+  let subset = true;
   astB.forEach((entity, name) => {
     // console.log(name)
     if (!isEntitySubset(astA.get(name), entity)) {
-      subset = false
+      subset = false;
     }
-  })    
-  return subset
+  });
+  return subset;
 };
 
 function isEntitySubset(A: Entity | undefined, B: Entity | undefined): boolean {
   if (!A) {
-    return false
+    return false;
   }
   if (!B) {
-    return true
+    return true;
   }
 
   // console.log(A.name, B.name, A.entities, B.entities)
 
-  for(let fB of B.fields) {
+  for (let fB of B.fields) {
     // console.log(fB)
-    let exists = false
-    for (let fA of A.fields) {    
-      if (fA == fB) {
-        exists = true
-        break
+    let exists = false;
+    for (let fA of A.fields) {
+      if (fA === fB) {
+        exists = true;
+        break;
       }
     }
     if (!exists) {
-      return false
+      return false;
     }
   }
-
 
   for (let id of Array.from(B.entities.keys())) {
-    let entity = B.entities.get(id) 
+    let entity = B.entities.get(id);
     if (A.entities.has(id)) {
       if (!isEntitySubset(A.entities.get(id), entity)) {
-        return false
+        return false;
       }
     } else {
-      return false
+      return false;
     }
   }
-  
-  return true
+
+  return true;
 }
